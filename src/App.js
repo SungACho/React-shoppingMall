e683e8bd90e26e2ed6dbc18d829ae0fd43575651
SungACho <sunga0720@naver.com>
2022-03-28 15:31:35 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
+/* eslint-disable*/
+import React, { useState } from 'react';
+import { Navbar, Container, Nav, NavDropdown,Button } from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
+//import {name, name2} from './data.js';
+import Data from './data.js';
+import Detail from './Detail.js';
+
+import { Link, Route, Switch } from 'react-router-dom';
+
 
 function App() {
+
+  let [shoes, shoes변경] = useState(Data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link><Link to="/">Home</Link></Nav.Link>
+              <Nav.Link><Link to="/detail">Detail</Link></Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      
+      <Switch>
+      <Route exact path="/">
+        <div className='Jumbotron'>
+          <h1>20% Season Off</h1>
+          <p>
+            브ㄹ라블라
+          </p>
+          <p>
+            <Button variant="primary">버튼</Button>
+          </p>
+        </div>
+
+        <div className='container'>
+          <div className='row'>
+            {
+              shoes.map(function(shoe,i){
+                return(
+                  <Card shoes={shoe} key={i}/>
+                )
+              })
+            }   
+          </div>
+        </div>
+      </Route>
+      <Route path="/detail"> 
+        <Detail/>
+      </Route>
+      <Route path="/:id">
+          <div>아무거나 적었을 때 보여주세요</div>
+      </Route>
+
+      </Switch>
+      {/*  <Route path="/어쩌구" component={Card} ></Route>  */} 
+      {/*  <Route path="/어쩌구"><Card/></Route>  */} 
     </div>
   );
+}
+function Card(props){
+  return (
+    <div className='col-md-4'>
+      <img src={"https://codingapple1.github.io/shop/shoes" + (props.shoes.id +1) +".jpg"} width="100%"/>
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.content} & {props.shoes.price}</p>
+    </div>    
+  )
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,27 @@
+/* eslint-disable*/
+export default [
+    {
+      id : 0,
+      title : "White and Black",
+      content : "Born in France",
+      price : 120000
+    },
+  
+    {
+      id : 1,
+      title : "Red Knit",
+      content : "Born in Seoul",
+      price : 110000
+    },
+  
+    {
+      id : 2,
+      title : "Grey Yordan",
+      content : "Born in the States",
+      price : 130000
+    }
+  ] 
+
+  //var name = 'Kim';
+  //var name2 = 'Park';
+  //export default {name, name2}
